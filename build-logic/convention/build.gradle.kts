6da8d1c53f7f18c965a833148e3aa4131764e080
Jason Grife <jasongrife@hotmail.com>
2024-05-12 22:17:24 -0700

    Updating to latest version AGP 8.4.0 and Gradle Wrapper 8.6
    NOTE: Needed to update from Gradle JDK 17.0.10 -> 17.0.11 in order to be able to get the apps signing fingerprint for use by Google Maps to restrict map access to this app only.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -45,5 +45,6 @@ gradlePlugin {
         register("jvmKtor") {
             id = "milemarker.jvm.ktor"
             implementationClass = "JvmKtorConventionPlugin"
-        }    }
+        }
+    }
 }
