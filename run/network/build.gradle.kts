c861bde37e170e937ed32a695560e69c3909fcc6
Jason Grife <jasongrife@hotmail.com>
2024-05-17 11:47:50 -0700

    Implement Run API calls

--- run/network/build.gradle.kts
+++ run/network/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.milemarker.android.library)
+    alias(libs.plugins.milemarker.jvm.ktor)
 }
 
 android {
@@ -9,4 +10,6 @@ android {
 dependencies {
     implementation(projects.core.domain)
     implementation(projects.core.data)
+
+    implementation(libs.bundles.koin)
 }
---
+++ run/presentation/src/test/java/com/example/run/presentation/active_run/ActiveRunViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.run.presentation.active_run
+
+import org.junit.Assert.*
+import kotlin.test.Test
+
+class ActiveRunViewModelTest {
+
+    @Test
+    fun `test iu`() {
+
+    }
+}
