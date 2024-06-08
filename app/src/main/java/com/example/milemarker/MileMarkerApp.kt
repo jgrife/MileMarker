571491e8d76303a83d5ca3f3ffb0b3ff183da651
Jason Grife <jasongrife@hotmail.com>
2024-06-08 13:47:29 -0700

    Implementing WorkManager implementation in koin DI

--- app/src/main/java/com/example/milemarker/MileMarkerApp.kt
+++ app/src/main/java/com/example/milemarker/MileMarkerApp.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
@@ -31,6 +32,7 @@ class MileMarkerApp: Application() {
         startKoin {
             androidLogger()
             androidContext(this@MileMarkerApp)
+            workManagerFactory()
             modules(
                 appModule,
                 authDataModule,
